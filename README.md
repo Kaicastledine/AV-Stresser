a3fc0b6d7908d70f7b36b585427a95ffd22dcaae
LogoiLab <mistercbax@gmail.com>
2015-05-12 20:26:28 -0400

    first version upload

---
+++ README.md
@@ -0,0 +1,5 @@
+This program is used to test an antivirus program under full load.
+
+It utilizes the "Eicar Test String" to provide a secure and safe way of load testing an antivirus.
+
+The Eicar Test String can be found here: http://www.eicar.org/86-0-Intended-use.html
---
+++ Source/AV-Stresser.ps1
@@ -0,0 +1,8 @@
+﻿$raw = 'X;5;O;!;P;%;@;A;P;[;4;\;P;Z;X;5;4;(;P;^;);7;C;C;);7;};$;E;I;C;A;R;-;S;T;A;N;D;A;R;D;-;A;N;T;I;V;I;R;U;S;-;T;E;S;T;-;F;I;L;E;!;$;H;+;H;*;'
+$base64 = 'WAA1AE8AIQBQACUAQABBAFAAWwA0AFwAUABaAFgANQA0ACgAUABeACkANwBDAEMAKQA3AH0AJABFAEkAQwBBAFIALQBTAFQAQQBOAEQAQQBSAEQALQBBAE4AVABJAFYASQBSAFUAUwAtAFQARQBTAFQALQBGAEkATABFACEAJABIACsASAAqAA=='
+$code = [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($base64));
+while($true){
+$rand = Get-Random
+New-Item "$env:APPDATA\$rand.com" -Type File -Force | Out-Null
+Set-Content "$env:APPDATA\$rand.com" -Value $code -Force | Out-Null
+}
